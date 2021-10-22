01f8c923e291afc18d8d8b0821cc3dd9d2bf947c
Maykon <maykonhs@gmail.com>
2021-10-22 10:52:46 -0300

    group by date

--- src/modules/appointments/infra/http/controllers/ProfileAppointmentsController.ts
+++ src/modules/appointments/infra/http/controllers/ProfileAppointmentsController.ts
@@ -1,8 +1,52 @@
 import { Request, Response } from 'express';
-import { parseISO } from 'date-fns';
+import {
+  parseISO,
+  isSameDay,
+  format,
+  isEqual,
+  isToday,
+  isYesterday,
+  isTomorrow,
+} from 'date-fns';
 import { container } from 'tsyringe';
 
 import ListProfileAppointmentsService from '@modules/appointments/services/ListProfileAppointmentsService';
+import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
+
+interface GroupedAppointment {
+  date: string | Date;
+  appointments: Appointment[];
+}
+
+const groupByDate = (appointments: Appointment[]) => {
+  const response: GroupedAppointment[] = [];
+  appointments.forEach(appointment => {
+    // const date = format(appointment.start_date, 'dd/MM/yyyy');
+    if (response.length === 0) {
+      response.push({
+        date: appointment.start_date,
+        appointments: [appointment],
+      });
+    } else {
+      const ind = response.findIndex(item => {
+        return isSameDay(new Date(item.date), new Date(appointment.start_date));
+      });
+      if (ind < 0) {
+        response.push({
+          date: appointment.start_date,
+          appointments: [appointment],
+        });
+      } else {
+        response[ind].appointments.push(appointment);
+      }
+    }
+  });
+  return response.sort((a, b) => {
+    if (a.date < b.date) return -1;
+    if (a.date > b.date) return 1;
+    return 0;
+  });
+};
 
 export default class ProfileAppointmentsController {
   public async index(req: Request, res: Response): Promise<Response> {
@@ -26,18 +70,20 @@ export default class ProfileAppointmentsController {
     const parsedStartDate = startDate ? parseISO(startDate) : undefined;
     const parsedEndDate = endDate ? parseISO(endDate) : undefined;
 
-    const appointment = await listProfileAppointmentsService.execute({
-      userId,
-      groupId,
-      startDate: parsedStartDate,
-      endDate: parsedEndDate,
-      appointmentName,
-      appointmentDescription,
-      status,
-      location,
-      isPrivate: isPrivate === 'true',
-    });
+    const appointments: Appointment[] = await listProfileAppointmentsService.execute(
+      {
+        userId,
+        groupId,
+        startDate: parsedStartDate,
+        endDate: parsedEndDate,
+        appointmentName,
+        appointmentDescription,
+        status,
+        location,
+        isPrivate: isPrivate === 'true',
+      },
+    );
 
-    return res.json(appointment);
+    return res.json(groupByDate(appointments));
   }
 }
